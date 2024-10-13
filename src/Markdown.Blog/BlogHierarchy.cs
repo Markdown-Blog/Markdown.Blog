6ac904b270de68c72fea50d865f8b6694785ed5a
Zhi Liang <zhi.liang.pt@gmail.com>
2024-10-13 17:09:52 +0100

    Try to setup automatic CICD for nuget packages

---
+++ src/Markdown.Blog/BlogHierarchy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Markdown.Blog
+{
+	public class BlogHierarchy
+	{
+		public string Division { get; set; }
+		public string Category { get; set; }
+		public string SubCategory { get; set; }
+	}
+}
---
+++ src/Markdown.Blog/BlogMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Markdown.Blog
+{
+	public class BlogMetadata
+	{
+		public string FilePath { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime Date { get; set; }
+		public List<string> Tags { get; set; }
+		public string CoverImage { get; set; }
+		public BlogHierarchy Hierarchy { get; set; }
+	}
+}
