23849412871434c4ce923a6f7a8cc9d1a9b9e3e9
Zhi Liang <zhi.liang.pt@gmail.com>
2024-12-25 19:38:47 +0000

    BlogDataProcessor

---
+++ src/Markdown.Blog/BlogData.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using Markdown.Blog.Procedures;
+
+namespace Markdown.Blog
+{
+    /// <summary>
+    /// Represents a blog post with its content and metadata.
+    /// Extends BlogMetadata to include the actual content of the blog post.
+    /// This class is primarily used on the client side.
+    /// </summary>
+    public class BlogData : BlogMetadata
+    {
+        /// <summary>
+        /// Gets or sets the division or category of the blog post.
+        /// </summary>
+        public Division Division { get; set; }
+
+        /// <summary>
+        /// Gets or sets the raw Markdown content of the blog post.
+        /// </summary>
+        public string MdContent { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the markdown content has been loaded.
+        /// </summary>
+        public bool IsMdContentLoaded => MdContent != null;
+
+        /// <summary>
+        /// Loads the markdown content from the remote source and assigns it to MdContent.
+        /// </summary>
+        /// <param name="division">The division containing repository information.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public async Task<bool> LoadMdContent(Division division)
+        {
+            MdContent = await BlogRawContentProcessor.GetContentAsync(division, this);
+            return IsMdContentLoaded;
+        }
+
+        public BlogData(Division division, BlogMetadata metadata) : base()
+        {
+            // Copy all properties from metadata to this instance
+            Division = division;
+            FilePath = metadata.FilePath;
+            Title = metadata.Title;
+            Description = metadata.Description;
+            Date = metadata.Date;
+            Tags = metadata.Tags;
+            CoverImages = metadata.CoverImages;
+            Hierarchy = metadata.Hierarchy;
+            PathSegments = metadata.PathSegments;
+            MdContent = null;
+        }
+    }
+}
---
+++ src/Markdown.Blog/Procedures/BlogData.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Markdown.Blog.Procedures
+{
+    /// <summary>
+    /// Provides functionality for handling blog metadata operations
+    /// </summary>
+    public static class BlogDataProcessor
+    {
+        public static List<BlogData> GetBlogDataListFromIndexJson(Division division, string jsonContent)
+        {
+            var blogMetadataList =   BlogMetadataProcessor.DeserializeMetadata(jsonContent);
+            var blogDataList = new List<BlogData>();
+            foreach (var blogMetadata in blogMetadataList)
+            {
+                blogDataList.Add(new BlogData(division, blogMetadata));
+            }
+            return blogDataList;
+        }
+    }
+}
